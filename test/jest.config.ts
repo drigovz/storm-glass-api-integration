447e5ab2e6676143efc0575c49039289675bccd8
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-11 20:12:07 -0300

    feat: setup jest

---
+++ test/jest.config.ts
@@ -0,0 +1,12 @@
+const { resolve } = require('path'),
+    root = resolve(__dirname, '..'),
+    rootConfig = require(`${root}/jest.config.js`);
+
+module.exports = {
+    ...rootConfig, ...{
+        rootDir: root,
+        displayName: "end2end-tests",
+        setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+        testMatch: ["<rootDir>/test/**/*.test.ts"],
+    }
+}
