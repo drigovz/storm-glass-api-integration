3627c4e1e6f46d74af3b0b6563226e2afa3fe482
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-11 21:39:09 -0300

    feat: implemented firts integration test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast-controller';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -13,5 +15,15 @@ export class SetupServer extends Server {
 
     public init(): void {
         this.setupExpress();
+        this.setupControllers();
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+        this.addControllers([forecastController]);
+    }
+
+    public getApp(): Application {
+        return this.app;
     }
 };
